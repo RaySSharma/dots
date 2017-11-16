27e8ad9cc67748bb212cab908da3490e2381abc2
rayssharma <ramon.sharma1@gmail.com>
2017-11-16 11:56:56 -0500

    Updated color scheme, lockscreen

---
+++ .config/i3lock-fancier/lockscreen.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+SCRIPTPATH=$(realpath "$0")
+SCRIPTPATH=${SCRIPTPATH%/*}
+IMAGE="$SCRIPTPATH/lock.png"
+
+if [ ! -e "$IMAGE" ]; then
+	# Get copy of wallpaper
+	WALLP="/home/ray/Pictures/Wallpapers/stars.png"
+	cp $WALLP $IMAGE
+
+	# Scale it to fit screen
+	convert "$IMAGE" -resize 1920x1080^ -gravity center -extent 1920x1080 $IMAGE
+
+	# Add Lock Icon
+	ICON="$SCRIPTPATH/lock_white.png"
+	ICON="/tmp/lock_icon.png"
+	cp $ICON_ORIG $ICON
+	convert "$ICON" -resize 190x190 "$ICON"
+	convert "$IMAGE" "$ICON" -gravity center -composite "$IMAGE"
+fi
+
+i3lock
